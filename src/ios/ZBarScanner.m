5cbba4e5f1daac443abfcb4968e368fa92a07130
mrameezraja <mrameezraja@gmail.com>
2016-06-24 12:12:00 +0500

    restricted to portrait mode

--- src/ios/ZBarScanner.m
+++ src/ios/ZBarScanner.m
@@ -1,7 +1,7 @@
 
 #import <Foundation/Foundation.h>
 #import <AudioToolbox/AudioToolbox.h>
-#import "ZBarcodeScannerViewController"
+#import "ZBarcodeScannerViewController.h"
 #import "ZBarScanner.h"
 #import "ZBarSDK.h"
 
