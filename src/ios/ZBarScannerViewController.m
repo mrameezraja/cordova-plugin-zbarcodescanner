fa7ebdaebabc1f0662d3f533a9a01e0b9371b69e
mrameezraja <mrameezraja@gmail.com>
2016-06-24 11:37:44 +0500

    restricted to portrait mode

--- src/ios/ZBarScannerViewController.m
+++ src/ios/ZBarScannerViewController.m
@@ -15,7 +15,7 @@
     return UIInterfaceOrientationPortrait;
 }
 
-- (NSUInteger)supportedInterfaceOrientations
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations
 {
     return UIInterfaceOrientationMaskPortrait;
 }
